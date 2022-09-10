43f5ee5fcc2d58db81f9e95d67c9ec955eb3f2df
MatheusEzaquiel <matheusbezaquiel@gmail.com>
2022-09-10 01:23:14 -0300

    incorporação do carrossel e arquivos do bootstrap

--- config/conexao.php
+++ config/conexao.php
@@ -3,7 +3,7 @@ try{
     DEFINE("HOST","localhost");
     DEFINE("DB","bd_jovem_capacitado");
     DEFINE("USER","root");
-    DEFINE("PASSWORD","mmmm");
+    DEFINE("PASSWORD","thaina");
 
     $connect = new PDO ('mysql:host='.HOST.';dbname='.DB,USER,PASSWORD);
     $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
